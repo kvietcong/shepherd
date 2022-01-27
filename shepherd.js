0ede5e33183843722965d7a3a93d5c6d156f14e9
KV Le <32186663+kvietcong@users.noreply.github.com>
2022-01-26 17:18:55 -0800

    ⌨️ Move to Keyboard Centric Controls

--- shepherd.js
+++ shepherd.js
@@ -64,37 +64,45 @@ class Shepherd extends Entity {
         const {
         } = params.shepherd;
 
-        if (gameEngine.keys.ArrowRight) {
-            this.velocity.x += 1;
-            this.state = 1;
-            this.facing = 3;
-        }
-        if (gameEngine.keys.ArrowLeft) {
-            this.velocity.x -= 1;
-            this.state = 1;
-            this.facing = 1;
-        }
-        if (gameEngine.keys.ArrowUp) {
+        const {
+            w, a, s, d, q, e,
+            ArrowRight: right, ArrowLeft: left, ArrowUp: up, ArrowDown: down,
+            Shift, Control,
+        } = gameEngine.keys;
+        const space = gameEngine.keys[" "];
+
+        if (w) {
             this.velocity.y -= 1;
             this.state = 1;
             this.facing = 0;
         }
-        if (gameEngine.keys.ArrowDown) {
+        if (a) {
+            this.velocity.x -= 1;
+            this.state = 1;
+            this.facing = 1;
+        }
+        if (s) {
             this.velocity.y += 1;
             this.state = 1;
             this.facing = 2;
         }
+        if (d) {
+            this.velocity.x += 1;
+            this.state = 1;
+            this.facing = 3;
+        }
 
-        const { z, x, c } = gameEngine.keys;
         let isAttacking = false;
-        if (z || x || c) {
-            if (z) this.state = 2;
-            if (x) this.state = 3;
-            if (c) this.state = 4;
+        if (space || q || e) {
+            if (q) this.state = 2;
+            if (space) this.state = 3;
+            if (e) this.state = 4;
             isAttacking = true;
         }
 
-        this.velocity.setUnit().scaleInPlace(this.maxSpeed * (isAttacking ? 0.6 : 1));
+        this.velocity
+            .setUnit()
+            .scaleInPlace(this.maxSpeed * (isAttacking ? 0.5 : 1));
 
         this.x += this.velocity.x * gameEngine.deltaTime;
         this.y += this.velocity.y * gameEngine.deltaTime;
