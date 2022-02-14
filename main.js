740a6a0216ba306b30416f267ac3e02ce29aa296
Mason Hansen <masonh6@uw.edu>
2022-02-14 14:08:09 -0800

    🗺new map and other stuff

--- main.js
+++ main.js
@@ -29,6 +29,10 @@ assetManager.queueDownload("./resources/1.png");
 assetManager.queueDownload("./resources/2.png");
 assetManager.queueDownload("./resources/3.png");
 assetManager.queueDownload("./resources/starDewBuildings.png");
+assetManager.queueDownload("./resources/forestwater.png");
+assetManager.queueDownload("./resources/big_tree.png");
+assetManager.queueDownload("./resources/treetrunk.png");
+assetManager.queueDownload("./resources/forestground.png");
 assetManager.queueDownload("./resources/Map_tiles.png");
 assetManager.queueDownload("./resources/slash.png")
 assetManager.queueDownload("./resources/fence_00.png")
@@ -41,7 +45,7 @@ assetManager.downloadAll(() => {
 	gameEngine.init(ctx);
 
 	const entities = [];
-	const shepherd = new Shepherd(1700, 900);
+	const shepherd = new Shepherd(1200, 800);
 	//const shepherd = new Shepherd(canvas.width / 2, canvas.height / 2);
 	params.debugEntities.shepherd = shepherd;
 
@@ -54,10 +58,74 @@ assetManager.downloadAll(() => {
 		}
 		entities.push(new Sheep(x, y));
 	}
+	
+	//starting area collision boxes
+	//west
+	entities.push(new Obstacle(950, 550, "./resources/3.png", 50, 50, 15, 200, 5));
+	//north
+	entities.push(new Obstacle(950, 550, "./resources/3.png", 50, 50, 400, 15, 5));
+	//east
+	entities.push(new Obstacle(2880, 550, "./resources/3.png", 50, 50, 15, 200, 5));
+	//there are two southern collision boxes to allow for an opening
+	//south west
+	entities.push(new Obstacle(950, 1480, "./resources/3.png", 50, 50, 150, 15, 5));
+	//south east
+	entities.push(new Obstacle(2200, 1480, "./resources/3.png", 50, 50, 150, 15, 5));
+
+	//first walkway collision boxes
+	//west
+	entities.push(new Obstacle(1650, 1500, "./resources/3.png", 50, 50, 15, 220, 5));
+	//east
+	entities.push(new Obstacle(2200, 1500, "./resources/3.png", 50, 50, 15, 140, 5));
+	//south
+	entities.push(new Obstacle(1650, 2600, "./resources/3.png", 50, 50, 423, 15, 5));
+	//north
+	entities.push(new Obstacle(2200, 2140, "./resources/3.png", 50, 50, 312, 15, 5));
+
+	//main area collision boxes
+	//south
+	entities.push(new Obstacle(3700, 2700, "./resources/3.png", 50, 50, 495, 15, 5));
+	//west
+	entities.push(new Obstacle(3675, 500, "./resources/3.png", 50, 50, 15, 342, 5));
+	//north
+	entities.push(new Obstacle(3675, 480, "./resources/3.png", 50, 50, 890, 15, 5));
+	//east
+	entities.push(new Obstacle(6090, 1100, "./resources/3.png", 50, 50, 15, 342, 5));
+
+
+	//forest map obstacles/decorations
+	//starting area
+	//big tree in the west
+	entities.push(new Obstacle(100, 230, "./resources/big_tree.png", 0, 0, 281, 255, 3));
+	//well in the starting area
+	entities.push(new Obstacle(2500, 900, "./resources/starDewBuildings.png", 453, 33, 47, 74, 2));
+	//water surrounding well
+	entities.push(new Obstacle(2250, 630, "./resources/forestwater.png", 0, 0, 319, 319, 2));
+
+	//first walkway area
+	//rocks
+	entities.push(new Obstacle(2000, 2100, "./resources/2.png"));
+	entities.push(new Obstacle(1850, 1700, "./resources/1.png"));
+	//tree stump in the west
+	entities.push(new Obstacle(950, 2000, "./resources/treetrunk.png", 0, 0, 143, 122, 3));
+
+	//main area
+	//tree stump in the south east
+	entities.push(new Obstacle(5675, 2350, "./resources/treetrunk.png", 0, 0, 143, 122, 3));
+	//north west houses
+	entities.push(new Obstacle(3900, 500, "./resources/starDewBuildings.png", 0, 267, 456, 145, 2.5));
+
+	//ending/barn area
+	//barn
+	entities.push(new Obstacle(7500, 1200, "./resources/starDewBuildings.png", 130, 0, 111, 105, 3));
+	//silo
+	entities.push(new Obstacle(7800, 1130, "./resources/starDewBuildings.png", 390, 0, 47, 127, 3));
+	//green house
+	entities.push(new Obstacle(6200, 1900, "./resources/starDewBuildings.png", 483, 241, 111, 159, 3));
+
+
 	/*
 	//sample obstacles
-	let house = new Obstacle(195, 99, "./resources/3.png");
-	entities.push(house);
 	let house2 = new Obstacle(387, 380, "./resources/3.png");
 	entities.push(house2);
 	entities.push(new Obstacle(290, 860, "./resources/2.png"));
@@ -136,6 +204,36 @@ assetManager.downloadAll(() => {
 		width: 32,
 		image: forestWaterImage
 	}
+	let rockTile = {
+		x: 162,
+		y: 590,
+		width: 40,
+		image: forestSourceImage
+	}
+	let vrockTile = {
+		x: 46,
+		y: 480,
+		width: 40,
+		image: forestSourceImage
+	}
+	let darkWaterTile = {
+		x: 190,
+		y: 105,
+		width: 32,
+		image: forestWaterImage
+	}
+	let startWaterTile = {
+		x: 0,
+		y: 160,
+		width: 32,
+		image: forestWaterImage
+	}
+	let waterRockTile = {
+		x: 321,
+		y: 193,
+		width: 32,
+		image: forestWaterImage
+	}
 	//assemble tileData array
 	let tileData = [];
 	tileData["default"] = grassTile;
@@ -147,6 +245,11 @@ assetManager.downloadAll(() => {
 	tileData["forest"] = forestTile;
 	tileData["forestFlower"] = forestFlowerTile;
 	tileData["forestMud"] = forestMudTile;
+	tileData["rock"] = rockTile;
+	tileData["vrock"] = vrockTile;
+	tileData["dwater"] = darkWaterTile;
+	tileData["swater"] = startWaterTile;
+	tileData["rwater"] = waterRockTile;
 
 	//find tile size
 	const tileWidth = 96;
